bcb3bdd415d3af895842da778a7b9e55c7fe1d96
smokyweb <smokyweb@gmail.com>
2026-05-04 12:57:26 -0400

    Add Node.js server for Coolify deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  const file = path.join(__dirname, 'index.html');
+  fs.readFile(file, (err, data) => {
+    if (err) { res.writeHead(500); res.end('Error'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`White Noise running on port ${PORT}`));
